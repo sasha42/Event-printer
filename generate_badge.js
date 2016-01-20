67432d3790741bce6f5415f0854b64f56fa20288
sasha42 <sasha42@users.noreply.github.com>
2016-01-20 13:29:55 +0100

    Improved barcode scanning, basic serial printing

--- generate_badge.js
+++ generate_badge.js
@@ -3,6 +3,6 @@ var system = require('system');
 var address = "http://localhost:5000/print?id=" + system.args[1];
 console.log(address)
 page.open(address, function() {
-  page.render('badge.png');
+  page.render('/event-printer/badge.png');
   phantom.exit();
 });
--- routes.py
+++ routes.py
@@ -1,28 +1,40 @@
 from flask import Flask, render_template, request
 import pyexcel as pe
 import subprocess
- 
+from PIL import Image
+
 app = Flask(__name__)
 sheet = pe.get_sheet(file_name="orders.csv", name_rows_by_column=0)
- 
+
+def serial_print(orderid):
+	return orderird
+
+def fetch_order(orderid):
+	id = orderid[:9]
+	order = sheet.row[id]
+	return(id, order)
+
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('layout.html')
 
 @app.route('/badge/', defaults={'name':'test','skills':'Dev'})
 @app.route('/badge/<name>/<skills>/')
 def badge(name, skills):
 	return render_template('badge.html', name=name, skills=skills)
 
-@app.route('/scan')
+@app.route('/scan', methods=['GET', 'POST'])
 def scan():
-	orderid = request.args.get('id')
-	sanitized_id = orderid[:9]
-	order = sheet.row[sanitized_id]
-	if order[4] == "Hackathon admission":
-		order[4] = "Hackathon participant"
-	subprocess.call(["/event-printer/phantomjs", "/event-printer/generate_badge.js", "483915543"])
-	return render_template('badge.html', name=order[0]+" "+order[1], skills=order[4])
+	if request.method == 'POST':
+		order = fetch_order(request.form['id'])
+		subprocess.call(["/event-printer/phantomjs", "/event-printer/generate_badge.js", order[0]])
+		badge = Image.open("/event-printer/badge.png")
+		badge = badge.rotate(270)
+		badge.save("/event-printer/badge2.png")
+		subprocess.call(["/scripts/ql570/ql570", "/dev/usb/lp0", "w", "/event-printer/badge2.png"])
+		return render_template('scanner.html', url=request.url_root)
+	else:
+		return render_template('scanner.html', url=request.url_root)
 
 @app.route('/print')
 def prtinty():
